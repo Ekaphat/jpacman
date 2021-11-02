30df3fe0b21a506dff0be83314bd0634d5686dff
EKP <55126755+Ekaphat@users.noreply.github.com>
2021-11-02 15:33:36 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,11 +3,16 @@ package nl.tudelft.jpacman.board;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import org.junit.jupiter.api.Test;
-
+/**
+ * A very simple (and not particularly useful)
+ * test class to have a starting point where to put tests.
+ *
+ * @author Arie van Deursen
+ */
 public class BoardTest {
-/*
-
-*/
+    /**
+     * Do we get the correct delta when moving north?
+     */
     @Test
     void testboard() {
         Square[][] oneoneBoard = {
