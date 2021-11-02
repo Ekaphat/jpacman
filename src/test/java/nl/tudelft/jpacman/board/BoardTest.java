237ef9bdf1b85fbbb6522d241f97272c59999379
EKP <55126755+Ekaphat@users.noreply.github.com>
2021-11-02 15:03:09 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,7 +3,9 @@ package nl.tudelft.jpacman.board;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import org.junit.jupiter.api.Test;
+/*
 
+*/
 public class BoardTest {
 
     @Test
